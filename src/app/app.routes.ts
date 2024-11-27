04e0dd62f9be42b0d9defb814031b8a5559a9596
Matheus Gomes <matheusprgc@gmail.com>
2024-11-27 09:24:34 -0300

    feat: Todoas as rotas que não existem será redirecionada para o componente welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
-import { LOGIN_ROUTES } from './pages/login/login.routes';
 
 export const routes: Routes = [
-
+  { path: '*', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
 ];
 
