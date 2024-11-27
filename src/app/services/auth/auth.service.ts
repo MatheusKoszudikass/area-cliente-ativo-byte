f3ad0dad71f08bdd5df64a71a286628ba7951f69
Matheus Gomes <matheusprgc@gmail.com>
2024-11-27 09:33:45 -0300

    feat: Headers passando o tipo do arquivo a necessidade do credentials para o JWT.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -5,13 +5,14 @@ import { lastValueFrom } from 'rxjs';
 import { ResponseLogin } from '../../interfaces/response-login.interface';
 import { CreateLogin } from '../../interfaces/create-login.interface';
 import { environment } from '../../../environments/environment';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isLoggedIn: boolean = false;
-  apiUrl:string = environment.apiLogin;
+  apiUrl: string = environment.apiLogin;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -19,33 +20,37 @@ export class AuthService {
     withCredentials: true
   };
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient, private notification: NzNotificationService) { }
 
   async login(loginData: CreateLogin): Promise<ResponseLogin> {
-      const response = await lastValueFrom(this.http.post<ResponseLogin>(`${this.apiUrl}/api/login`, loginData, this.httpOptions));
-      this.verifyLogin(response);
-      return response;
+    const response = await lastValueFrom(this.http.post<ResponseLogin>(`${this.apiUrl}/api/login`, loginData, this.httpOptions));
+    this.verifyLogin(response);
+    return response;
   }
 
   verifyLogin(response: ResponseLogin) {
     if (response.status) {
-      console.log("Login bem-sucedido. Dados:", response.data);
+      this.notification.create(
+        'success',
+        'Login bem-sucedido!',
+        `Login efetuado com sucesso!`
+      );
       this.router.navigate(['/welcome']);
     } else {
-      console.warn("Login falhou.");
-      response.message = 'Usuário ou senha inválidos!';
-      alert(response.message);
+      this.notification.create(
+        'error',
+        'Não foi possível efetuar o login',
+        response.message
+      );
     }
   }
 
-  async isVerifyToken(): Promise<boolean> {
+  private async isVerifyToken(): Promise<boolean> {
     try {
 
       const response = await lastValueFrom(this.http.get<boolean>(
         `${this.apiUrl}/api/auth/verify`, this.httpOptions));
-
       return response;
-
     } catch (error) {
       return false;
     }
@@ -57,7 +62,7 @@ export class AuthService {
     this.router.navigate(['/login']);
   }
 
- async isAuthenticated(): Promise<boolean> {
+  async isAuthenticated(): Promise<boolean> {
     return await this.isVerifyToken();
   }
 }
