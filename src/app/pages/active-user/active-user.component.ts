f679c15686e97ed84aaed93bf51bf068b7b9618c
Matheus Gomes <matheusprgc@gmail.com>
2024-12-02 19:23:37 -0300

    feat: Assim que inicialiar o componente chama o serviço auth com a função activeUser. É redireciona o usuário para a página de login.

--- src/app/pages/active-user/active-user.component.ts
+++ src/app/pages/active-user/active-user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { ActivatedRoute, Router  } from '@angular/router';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-active-user',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './active-user.component.scss'
 })
 export class ActiveUserComponent {
+ constructor(private authService: AuthService, private routerActive: ActivatedRoute, private router: Router ) { }
 
+ ngOnInit(): void {
+  const token = this.routerActive.snapshot.queryParamMap.get('token');
+
+  if(token != null)this.authService.activeUser(token);
+  setTimeout(() => {
+    this.router.navigate(['/login']);
+  }, 3000);
+ }
 }
