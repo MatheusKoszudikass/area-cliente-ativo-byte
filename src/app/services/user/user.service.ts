6be9afb95635650f57917120c674eeac02aafdeb
MatheusKoszudikass <matheusprgc@gmail.com>
2024-12-15 06:10:51 -0300

    feat: Serviço do usuário.

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { environment } from '../../../environments/environment';
+import { ResponseApi } from '../../interfaces/response-api.interface';
+import { CreateUserInterface } from '../../interfaces/create-user-interface';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl: string = environment.apiLogin
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  }
+  constructor(private router: Router, private http: HttpClient,
+    private notification: NzNotificationService
+  ) { }
+
+  public async add(User: CreateUserInterface): Promise<ResponseApi | null> {
+    try {
+      const response = await lastValueFrom(this.http.post<ResponseApi>(`${this.apiUrl}/api/user/add`, User, this.httpOptions));
+      return response;
+    } catch (error) {
+      this.notification.create('error', 'API', 'Desculpe,' +
+        ' ocorreu um erro ao processar sua solicitação. Por favor, ' +
+        'tente novamente mais tarde ou contate nosso suporte para obter ajuda.');
+    }
+    return null
+  }
+}
