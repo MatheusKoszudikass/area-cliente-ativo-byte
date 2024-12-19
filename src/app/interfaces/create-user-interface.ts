71000ae3434921f5b1b1e21caeccd04336842a02
MatheusKoszudikass <matheusprgc@gmail.com>
2024-12-19 08:57:51 -0300

    refactor: Propriedades agora pode ser nulo e indefinido

--- src/app/interfaces/create-user-interface.ts
+++ src/app/interfaces/create-user-interface.ts
@@ -1,14 +1,14 @@
 import { CreateRoleInterface } from "./create-role.interface";
 
 export interface CreateUserInterface {
-    email: string;
-    roles: Array<CreateRoleInterface>[];
-    firstname: string;
-    lastname: string;
-    username: string;
-    password: string;
-    cpfcnpj: string;
-    number: string;
-    legalPerson: boolean;
+    email: string| null | undefined;
+    password: string| null | undefined;
+    firstName: string| null | undefined;
+    lastName: string| null | undefined;
+    cnpjCpfRg: string| null | undefined;
+    legalRegister: boolean| null | undefined;
+    userName: string| null | undefined;
+    roles: Array<CreateRoleInterface>[] | null | undefined;
+    token: string| null | undefined;
     
 }
