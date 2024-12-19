02757a6f2effde718c4bdaff95ffe21d3c9d85c0
MatheusKoszudikass <matheusprgc@gmail.com>
2024-12-19 09:04:05 -0300

    feat: Novos metodos de validar documento cpf ou cnpj, verifica na api se o usuário ou email já é existente.

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -11,7 +11,7 @@ import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
   providedIn: 'root'
 })
 export class UserService {
-  apiUrl: string = environment.apiLogin
+  apiUrl: string = environment.apiLogin;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -21,15 +21,110 @@ export class UserService {
     private notification: NzNotificationService
   ) { }
 
-  public async add(User: CreateUserInterface): Promise<ResponseApi | null> {
+  public async add(user: CreateUserInterface): Promise<void> {
     try {
-      const response = await lastValueFrom(this.http.post<ResponseApi>(`${this.apiUrl}/api/user/add`, User, this.httpOptions));
-      return response;
+      const response = await lastValueFrom(this.http.post<ResponseApi>(`${this.apiUrl}/api/user/add`, user, this.httpOptions));
+      this.notificationOfUserRegistration(response);
+
     } catch (error) {
       this.notification.create('error', 'API', 'Desculpe,' +
         ' ocorreu um erro ao processar sua solicitação. Por favor, ' +
         'tente novamente mais tarde ou contate nosso suporte para obter ajuda.');
     }
-    return null
+  }
+
+  private notificationOfUserRegistration(response: ResponseApi): void {
+
+    if (response.success == true) {
+      this.notification.create(
+        'success',
+        'Usuário',
+        response.message,
+      );
+      this.router.navigate(['/login']);
+      return;
+    }
+    this.notification.create('error', 'Usuário', response.message);
+  }
+
+  public async Exist(identifier: string | null | undefined): Promise<boolean> {
+    try {
+      const response = await lastValueFrom(this.http.post<ResponseApi>(`${this.apiUrl}/api/user/exist`, { identifier }, this.httpOptions));
+      return response.success;
+    } catch (error) {
+      this.notification.create('error', 'API', 'Desculpe,' +
+        ' ocorreu um erro ao processar sua solicitação. Por favor, ' +
+        'tente novamente mais tarde ou contate nosso suporte para obter ajuda.');
+
+      return false
+    }
+  }
+
+
+  public validationDocument(documentType: string | null | undefined,
+    document: string | null | undefined): boolean {
+    if (!document || document.trim().length === 0) return false
+
+    if (documentType === 'CPF') {
+      return this.validationCpf(document);
+    }
+    return this.validationCnpj(document);
+  }
+
+  private validationCpf(cpf: string | null | undefined): boolean {
+
+    if (cpf?.length !== 11 || /^d(\d)\1{13}$/.test(cpf)) {
+      this.notification.create('error', 'Documento', 'CPF precisa de 11 dígitos');
+      return false
+    }
+    if (this.calculationValidationCPf(cpf)) {
+      return true
+    }
+    this.notification.create('error', 'Documento', 'CPF Inválido ');
+    return false
+  }
+
+  private calculationValidationCPf(cpf: string): boolean {
+
+    const calc = (x: number): number => {
+      const total = cpf
+        .substring(0, x)
+        .split('')
+        .reduce((sum, num, index) => sum + parseInt(num) * (x + 1 - index), 0);
+      const resto = total % 11;
+      return resto < 2 ? 0 : 11 - resto;
+    };
+
+    return calc(9) === parseInt(cpf[9]) && calc(10) === parseInt(cpf[10]);
+  }
+
+  private validationCnpj(cnpj: string | null | undefined): boolean {
+
+    if (cnpj?.length !== 14 || /^(\d)\1+$/.test(cnpj)) {
+      this.notification.create('error', 'Documento', 'CNPJ precisa de 14 dígitos');
+      return false
+    }
+
+    if (this.calculationValidationCnpj(cnpj)) {
+
+      return true
+    }
+
+    this.notification.create('error', 'Documento', 'CNPJ Inválido ');
+    return false
+  }
+
+  private calculationValidationCnpj(cnpj: string): boolean {
+    const calc = (x: number): number => {
+      const c = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+      const total = cnpj
+        .substring(0, x)
+        .split('')
+        .reduce((sum, num, index) => sum + parseInt(num) * c[13 - x + index], 0);
+      const resto = total % 11;
+      return resto < 2 ? 0 : 11 - resto;
+    };
+
+    return calc(12) === parseInt(cnpj[12]) && calc(13) === parseInt(cnpj[13]);
   }
 }
