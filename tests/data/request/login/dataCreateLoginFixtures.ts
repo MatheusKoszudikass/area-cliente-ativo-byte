d699da056ed9282dfe66209057023285aced025f
MatheusKoszudikass <matheusprgc@gmail.com>
2025-01-10 07:26:22 -0300

    feat: Dados fictício de login para teste do serviço auth.

---
+++ tests/data/request/login/dataCreateLoginFixtures.ts
@@ -0,0 +1,13 @@
+import { CreateLogin } from "../../../../src/app/interfaces/create-login.interface";
+
+export const REQUEST_CREATE_LOGIN_VALID_JSON:  CreateLogin = {
+    "emailUserName": "H4H6o@example.com",
+    "password": "12345678",
+    "remember": true
+}
+
+export const REQUEST_CREATE_LOGIN_INVALID_JSON: CreateLogin = {
+    "emailUserName": "",
+    "password": "12345678",
+    "remember": true
+}
