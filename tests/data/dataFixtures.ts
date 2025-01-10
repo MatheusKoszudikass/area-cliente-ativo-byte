5bb3cc973a22397246579e9abb29020852c95ad4
MatheusKoszudikass <matheusprgc@gmail.com>
2025-01-10 07:23:58 -0300

    feat: Dados fictícios gerais para testes da aplicação.

---
+++ tests/data/dataFixtures.ts
@@ -0,0 +1,5 @@
+export const RESPONSE_INVALID_API_JSON: [string, string, string] = [
+    'error',
+    'API',
+    'Desculpe, ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde ou contate nosso suporte para obter ajuda.'
+  ];
