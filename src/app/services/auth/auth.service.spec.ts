2896d1a4d132f4826137e279c71631e0f4d2e56c
MatheusKoszudikass <matheusprgc@gmail.com>
2025-01-15 15:16:07 -0300

    feat: Dependencia. Teste unitário método login

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -1,21 +1,25 @@
 import { fakeAsync, flush, TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHeaders, provideHttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { environment } from '../../../environments/environment';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { NOTIFICATION_EMAIL_EMPTY_RECOVERY_JSON, NOTIFICATION_INVALID_ACTIVE_USER_JSON, NOTIFICATION_VALID_ACTIVE_USER_JSON, NOTIFICATION_VALID_RECOVERY_JSON} from '../../../../tests/data/notification/auth/authNotificationFixture';
-import { RESPONSE_INVALID_ACTIVE_USER_JSON, RESPONSE_VALID_ACTIVE_USER_JSON, RESPONSE_VALID_RECOVERY_ACCOUNT_JSON } from '../../../../tests/data/response/auth/authResponseDataFixtures';
+import { ResponseApi } from '../../interfaces/response-api.interface';
+import { NOTIFICATION_EMAIL_EMPTY_RECOVERY_JSON, NOTIFICATION_INVALID_ACTIVE_USER_JSON, NOTIFICATION_INVALID_LOGIN_JSON, NOTIFICATION_VALID_ACTIVE_USER_JSON, NOTIFICATION_VALID_LOGIN_JSON, NOTIFICATION_VALID_RECOVERY_JSON } from '../../../../tests/data/notification/auth/authNotificationFixture';
+import { RESPONSE_INVALID_ACTIVE_USER_JSON, RESPONSE_INVALID_LOGIN_JSON, RESPONSE_VALID_ACTIVE_USER_JSON, RESPONSE_VALID_LOGIN_JSON, RESPONSE_VALID_RECOVERY_ACCOUNT_JSON } from '../../../../tests/data/response/auth/authResponseDataFixtures';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RESPONSE_INVALID_API_JSON } from '../../../../tests/data/dataFixtures';
 import { REQUEST_CREATE_LOGIN_INVALID_JSON, REQUEST_CREATE_LOGIN_JSON, REQUEST_CREATE_LOGIN_VALID_JSON } from '../../../../tests/data/request/login/dataCreateLoginFixtures';
+import { Router } from '@angular/router';
+import { RESPONSE_HEADERS_TOKEN_SESSION_JSON } from '../../../../tests/dependencies/headers';
 
 
 describe('AuthService', () => {
   let service: AuthService;
   let httpMock: HttpTestingController;
   let notificationMock = jasmine.createSpyObj('NzNotificationService', ['create']);
+  let routerMock = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,13 +27,17 @@ describe('AuthService', () => {
       providers: [provideHttpClient(), AuthService, provideHttpClientTesting(), {
         provide: NzNotificationService,
         useValue: notificationMock
+      },
+      {
+        provide: Router,
+        useValue: routerMock
       }],
     });
 
     httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(AuthService);
   });
-
+  
   describe('activeUser', () => {
     it('should activate user with invalid token', fakeAsync(() => {
       service.activeUser('invalidToken').then(response => {
@@ -57,7 +65,7 @@ describe('AuthService', () => {
       expect(request.request.headers.get('Set-Cookie')).toBeNull();
 
       request.flush(RESPONSE_INVALID_ACTIVE_USER_JSON);
-      
+
       flush();
     }));
 
@@ -69,7 +77,7 @@ describe('AuthService', () => {
           NOTIFICATION_VALID_ACTIVE_USER_JSON.message
         );
       });
-  
+
       const request = httpMock.expectOne(req =>
         req.method === 'POST' &&
         req.url === `${environment.apiLogin}/api/user/twoFactorAuth` &&
@@ -77,12 +85,12 @@ describe('AuthService', () => {
         req.headers.get('Content-Type') === 'application/json' &&
         req.withCredentials === true
       );
-  
+
       expect(request.request.method).toBe('POST');
       expect(request.request.body).toBeNull();
       expect(request.request.headers.get('Content-Type')).toBe('application/json');
       expect(request.request.withCredentials).toBeTrue();
-  
+
       request.flush(RESPONSE_VALID_ACTIVE_USER_JSON);
 
       flush();
@@ -95,7 +103,7 @@ describe('AuthService', () => {
 
         )
       });
-  
+
       const request = httpMock.expectOne(req =>
         req.method === 'POST' &&
         req.url === `${environment.apiLogin}/api/user/twoFactorAuth` &&
@@ -103,12 +111,12 @@ describe('AuthService', () => {
         req.headers.get('Content-Type') === 'application/json' &&
         req.withCredentials === true
       );
-  
+
       expect(request.request.method).toBe('POST');
       expect(request.request.body).toBeNull();
       expect(request.request.headers.get('Content-Type')).toBe('application/json');
       expect(request.request.withCredentials).toBeTrue();
-  
+
       request.flush(
         RESPONSE_INVALID_API_JSON.type
       );
@@ -127,7 +135,7 @@ describe('AuthService', () => {
         );
       });
     }));
-      
+
     it('should initiate account recovery with valid login details', fakeAsync(() => {
       service.recoveryAccount(REQUEST_CREATE_LOGIN_VALID_JSON).then(response => {
         expect(response).toBeNull();
@@ -172,18 +180,88 @@ describe('AuthService', () => {
       );
       expect(request.request.method).toBe('POST');
       expect(request.request.body).toBe(REQUEST_CREATE_LOGIN_VALID_JSON);
-      request.flush(null, {status: 500, statusText: 'Internal Server Error'});
+      request.flush(null, { status: 500, statusText: 'Internal Server Error' });
     }));
   });
 
   describe('login', () => {
-    it('should authenticate user with valid credentials', async () => {
-      // Test implementation for login method
-    });
+    it('should authenticate user with valid credentials', fakeAsync(() => {
+      service.login(REQUEST_CREATE_LOGIN_VALID_JSON).then(response => {
+        expect(response).toBeUndefined();
+        expect(notificationMock.create).toHaveBeenCalledWith(
+          NOTIFICATION_VALID_LOGIN_JSON.type,
+          NOTIFICATION_VALID_LOGIN_JSON.title,
+          NOTIFICATION_VALID_LOGIN_JSON.message
+        );
+      });
 
-    it('should handle error during login', async () => {
-      // Test implementation for error handling in login method
-    });
+      const request = httpMock.expectOne(req =>
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/auth/login` &&
+        req.body === REQUEST_CREATE_LOGIN_VALID_JSON &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toBe('POST');
+      expect(request.request.body).toBe(REQUEST_CREATE_LOGIN_VALID_JSON);
+      expect(request.request.headers.get('Content-Type')).toBe('application/json');
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(RESPONSE_VALID_LOGIN_JSON, { headers: RESPONSE_HEADERS_TOKEN_SESSION_JSON });
+      flush();
+    }));
+
+    it('should authenticate user with invalid credentials', fakeAsync(() => {
+      service.login(REQUEST_CREATE_LOGIN_INVALID_JSON).then(response => {
+        expect(response).toBeUndefined();
+        expect(notificationMock.create).toHaveBeenCalledWith(
+          NOTIFICATION_INVALID_LOGIN_JSON.type,
+          NOTIFICATION_INVALID_LOGIN_JSON.title,
+          NOTIFICATION_INVALID_LOGIN_JSON.message
+        );
+      });
+
+      const request = httpMock.expectOne(req => 
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/auth/login` &&
+        req.body === REQUEST_CREATE_LOGIN_INVALID_JSON &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toBe('POST');
+      expect(request.request.body).toBe(REQUEST_CREATE_LOGIN_INVALID_JSON);
+      expect(request.request.headers.get('Content-Type')).toBe('application/json');
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(RESPONSE_INVALID_LOGIN_JSON);
+      flush();
+     }));
+
+    it('should handle error during login', fakeAsync(() => {
+      service.login(REQUEST_CREATE_LOGIN_INVALID_JSON).then(error => {
+        expect(error).toBeUndefined();
+        expect(notificationMock.create).toHaveBeenCalledWith(
+          RESPONSE_INVALID_API_JSON.type,
+          RESPONSE_INVALID_API_JSON.title,
+          RESPONSE_INVALID_API_JSON.message
+        );
+      });
+
+      const request = httpMock.expectOne(req => 
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/auth/login` &&
+        req.body === REQUEST_CREATE_LOGIN_INVALID_JSON &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toBe('POST');
+      expect(request.request.body).toBe(REQUEST_CREATE_LOGIN_INVALID_JSON);
+      expect(request.request.headers.get('Content-Type')).toBe('application/json');
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(null, { status: 500, statusText: 'Internal Server Error' });
+      flush();
+    }));
   });
 
   describe('isVerifyToken', () => {
