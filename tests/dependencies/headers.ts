4b8f6ef08a6936a79e3e719685277164efeb498f
MatheusKoszudikass <matheusprgc@gmail.com>
2025-01-17 08:08:59 -0300

    feat: Novos dados ficticíos.

--- tests/dependencies/headers.ts
+++ tests/dependencies/headers.ts
@@ -4,6 +4,11 @@ export const RESPONSE_HEADERS_TOKEN_SESSION_JSON =  new HttpHeaders({
     'Set-Cookie': 'session=token; Secure; HttpOnly; Path=/; SameSite=None',
 });
 
+export const RESPONSE_EMPTY_HEADERS_JSON = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': ''
+ }); 
+
 export const RESPONSE_HEADERS_JSON = new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': 'Session token'
