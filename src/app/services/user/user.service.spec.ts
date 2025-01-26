f9b1b8b360f85840dc9c36ccfc4b531ea82d4f1c
MatheusKoszudikass <matheusprgc@gmail.com>
2025-01-25 21:27:26 -0300

    tests: Funções do userService.

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -7,11 +7,13 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { REQUEST_INVALID_CREATE_USER_JSON, REQUEST_VALID_CREATE_USER_JSON } from '../../../../tests/data/request/user/dataCreateUserFixtures';
 import { environment } from '../../../environments/environment';
-import { REPONSE_INVALID_ADD_USER_JSON, RESPONSE_VALID_ADD_USER_JSON } from '../../../../tests/data/response/user/userResponseDataFixtures';
+import { REPONSE_INVALID_ADD_USER_JSON, RESPONSE_INVALID_EXIST_USER_JSON, RESPONSE_VALID_ADD_USER_JSON, RESPONSE_VALID_EXIST_USER_JSON, RESPONSE_VALID_FIND_USER_JSON } from '../../../../tests/data/response/user/userResponseDataFixtures';
 import { NOTIFICATION_INVALID_ADD_USER_JSON, NOTIFICATION_VALID_ADD_USER_JSON } from '../../../../tests/data/notification/user/userNotificationFixture';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { RESPONSE_INVALID_API_JSON } from '../../../../tests/data/dataFixtures';
+import { RESPONSE_EMPTY_HEADERS_JSON, RESPONSE_HEADERS_JSON } from '../../../../tests/dependencies/headers';
+import { identity } from 'rxjs';
 
 describe('UserService', () => {
   let service: UserService;
@@ -116,8 +118,152 @@ describe('UserService', () => {
       expect(request.request.withCredentials).toBeTrue();
 
       request.flush(null, { status: 500, statusText: 'Internal Server Error' });
+
       flush();
     }));
   });
+
+  describe('findUSerTokenSession', () => {
+    it('should call findUSerTokenSession with the valid', fakeAsync(() => {
+      service.findUserTokenSession().then(response => {
+        expect(response).toEqual(RESPONSE_VALID_FIND_USER_JSON);
+      })
+
+      const request = httpMock.expectOne(req =>
+        req.method === 'GET' &&
+        req.url === `${environment.apiLogin}/api/user/findUserSession` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toEqual('GET');
+      expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(request.request.withCredentials).toBeTrue();
+
+      request.flush(RESPONSE_VALID_FIND_USER_JSON, { headers: RESPONSE_HEADERS_JSON });
+
+      flush();
+    }));
+
+    it('should call findUserToken with the invalid', fakeAsync(() => {
+      service.findUserTokenSession().then(response => {
+        expect(response).toEqual(RESPONSE_VALID_FIND_USER_JSON);
+      })
+
+      const resquet = httpMock.expectOne(req =>
+        req.method === 'GET' &&
+        req.url === `${environment.apiLogin}/api/user/findUserSession` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(resquet.request.method).toEqual('GET');
+      expect(resquet.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(resquet.request.withCredentials).toBeTrue();
+      resquet.flush(RESPONSE_VALID_FIND_USER_JSON, { headers: RESPONSE_EMPTY_HEADERS_JSON });
+
+      flush();
+    }));
+
+    it('should call findUSerTokenSession with the error', fakeAsync(() => {
+      service.findUserTokenSession().then(response => {
+        expect(response).toBeNull();
+      })
+
+      const request = httpMock.expectOne(req =>
+        req.method === 'GET' &&
+        req.url === `${environment.apiLogin}/api/user/findUserSession` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toEqual('GET');
+      expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(null, { status: 500, statusText: 'Internal Server Error' });
+
+      flush();
+    }))
+  });
+
+  describe('existUser', () => {
+    it('should call exist with the valid', fakeAsync(() => {
+      const identitfier: string = 'email';
+      
+      service.existUser(identitfier).then(response => {
+        expect(response).toBeFalse();
+        expect(response).toEqual(RESPONSE_VALID_EXIST_USER_JSON.success);
+      });
+
+      const request = httpMock.expectOne(req => 
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/user/exist` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.body.identifier === identitfier &&
+        req.withCredentials === true
+      );
+      
+      expect(request.request.method).toEqual('POST');
+      expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(request.request.body.identifier).toEqual(identitfier);
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(RESPONSE_VALID_EXIST_USER_JSON);
+    
+      flush();
+    }));
+
+    it('should call exist with the invalid', fakeAsync(() => {
+      const identitfier: string = '';      
+      service.existUser(identitfier).then(response => {
+        expect(response).toBeTrue();
+        expect(response).toEqual(RESPONSE_INVALID_EXIST_USER_JSON.success);
+      });
+
+      const request = httpMock.expectOne(req => 
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/user/exist` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.body.identifier === identitfier &&
+        req.withCredentials === true
+      );
+      
+      expect(request.request.method).toEqual('POST');
+      expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(request.request.body.identifier).toEqual(identitfier);
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(RESPONSE_INVALID_EXIST_USER_JSON);
+    
+      flush();
+    }));
+
+    it('should call exist with the error', fakeAsync(() => {
+      const identitfier: string = 'email';
+      service.existUser(identitfier).then(response => {
+        expect(response).toBeFalse();
+        expect(nzNotificationMock.create).toHaveBeenCalledWith(
+          RESPONSE_INVALID_API_JSON.type,
+          RESPONSE_INVALID_API_JSON.title,
+          RESPONSE_INVALID_API_JSON.message
+        )
+      });
+
+      const request = httpMock.expectOne(req =>
+        req.method === 'POST' &&
+        req.url === `${environment.apiLogin}/api/user/exist` &&
+        req.headers.get('Content-Type') === 'application/json' &&
+        req.body.identifier === identitfier &&
+        req.withCredentials === true
+      );
+
+      expect(request.request.method).toEqual('POST');
+      expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(request.request.body.identifier).toEqual(identitfier);
+      expect(request.request.withCredentials).toBeTrue();
+      request.flush(null, { status: 500, statusText: 'Internal Server Error' });
+    
+      flush();
+    }));
+  });
+
   
 });
